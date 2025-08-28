00d66da157189cc064ccc8fb76c1208758d1dd8c
WinLakeLee <lst2413@gmail.com>
2025-08-28 09:18:15 +0900

    초기설정

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -4,30 +4,30 @@ import axiosInstance from "../axiosInstance";
 import './css/signup.css';
 
 const Signup = () => {
-   const [member, setMember] = useState({
+  const [member, setMember] = useState({
     username: '',
     password: '',
     email: ''
-   });
+  });
   const navigate = useNavigate();
 
   const onChangeHandler = (e) => {
     setMember({
-      ...setMember,
+      ...member,
       [e.target.name]: e.target.value
     })
   }
-  
-   return (
+
+  return (
     <div className="signup-container">
       <h2>회원가입</h2>
       <hr />
-      아이디 : <input type="text" name="username" onChange={onChangeHandler}/> <br />
-      닉네임 : <input type="text" name="nickname" onChange={onChangeHandler}/> <br />
-      비밀번호 : <input type="password" name="password" onChange={onChangeHandler}/> <br />
-      이메일 : <input type="text" name="email" onChange={onChangeHandler}/> <br />
+      아이디 : <input type="text" name="username" onChange={onChangeHandler} /> <br />
+      닉네임 : <input type="text" name="nickname" onChange={onChangeHandler} /> <br />
+      비밀번호 : <input type="password" name="password" onChange={onChangeHandler} /> <br />
+      이메일 : <input type="text" name="email" onChange={onChangeHandler} /> <br />
       <button onClick={() => {
-        axiosInstance.post('/signup',member)
+        axiosInstance.post('/signup', member)
           .then(response => {
             console.log(response)
             alert(response.data);
