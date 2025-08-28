e154f1e16a3ba81cfd85be8684a799441af03f55
panicbaek <woosikbaek@gmail.com>
2025-08-28 12:39:33 +0900

    modify

--- src/MyPage.jsx
+++ src/MyPage.jsx
@@ -1,8 +1,14 @@
 const MyPage = ({ userInfo }) => {
   
   return (
-    <div>
-      <p>{userInfo.nickname}님의 마이페이지</p>
+    <div className="login-container">
+      <h2>xxxx님</h2>
+      <hr />
+      <label>닉네임</label> <br />
+      <label>점수</label> <br />
+
+      <button>수정</button>
+      <button>탈퇴</button>
     </div>
   )
 }
