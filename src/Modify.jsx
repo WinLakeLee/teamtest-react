d84761b6a43f91ededc9a131346f9df85ef54527
panicbaek <woosikbaek@gmail.com>
2025-08-28 15:45:48 +0900

    mypage수정

--- src/Modify.jsx
+++ src/Modify.jsx
@@ -1,20 +1,54 @@
+import { useNavigate } from "react-router-dom";
 import axiosInstance from "../axiosInstance";
+import { useState } from "react";
 
 const Modify = ({ userInfo, setUserInfo }) => {
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const onChangeHandler = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const onSubmitHandler = () => {
+    if (password.length < 4) {
+      alert("비밀번호는 3자 이상이어야 합니다.");
+      return;
+    }
+
+    axiosInstance
+      .put(`/update`, { password })
+      .then((response) => {
+        console.log(response.data);
+
+        
+        setUserInfo({ ...userInfo, password });
+
+        navigate("/"); 
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("수정 중 오류가 발생했습니다.");
+      });
+  };
 
   return (
-    <>
-      <div className="login-container">
-        <h2>{userInfo}님</h2>
-        <label>{userInfo}</label> <br />
-        <label><input type="password" name="password"  placeholder="비밀번호를 입력하세요" /></label> <br />
+    <div className="login-container">
+      <h2>님</h2>
+      <label>
+        <input
+          type="password"
+          name="password"
+          placeholder="비밀번호를 입력하세요"
+          value={password}
+          onChange={onChangeHandler}
+        />
+      </label>
+      <br />
 
-        <button onClick={() => {
-          axiosInstance.post
-        }}>수정</button>
+      <button onClick={onSubmitHandler}>수정</button>
     </div>
-    </>
-  )
-}
+  );
+};
 
 export default Modify;
--- src/MyPage.jsx
+++ src/MyPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import axiosInstance from "../axiosInstance";
 
 const MyPage = ({ userInfo, setUserInfo}) => {
 
@@ -7,7 +8,7 @@ const MyPage = ({ userInfo, setUserInfo}) => {
 
   return (
     <div className="login-container">
-      <h2>xxxx님</h2>
+      <h2>님</h2>
       <hr />
       <label>닉네임</label> <br />
       <label>이메일</label> <br />
@@ -19,9 +20,15 @@ const MyPage = ({ userInfo, setUserInfo}) => {
         수정
       </button>
       <button className="deleteId" onClick={() => {
+        axiosInstance.delete('/delete')
+          .then(response => {
+            console.log(response.data);
+            alert("탈퇴 하시겠습니까?")
+            navigate("/")
+          }) .catch(error => {
+            console.log(error);
+          })
         
-        alert("탈퇴 하시겠습니까?")
-        navigate("/")
       }}>탈퇴</button>
     </div>
   )
