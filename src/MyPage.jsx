92a96f5ca714882ca5355bcd2b01a0a95d98ba89
panicbaek <woosikbaek@gmail.com>
2025-08-28 17:43:53 +0900

    리액트 mypage수정

--- src/MyPage.jsx
+++ src/MyPage.jsx
@@ -8,28 +8,36 @@ const MyPage = ({ userInfo, setUserInfo}) => {
 
   return (
     <div className="login-container">
-      <h2>{userInfo}님</h2>
+      <h2>{userInfo.username}님</h2>
       <hr />
-      <label>닉네임</label> <br />
-      <label>이메일</label> <br />
-      <label>점수</label> <br />
+      <label>{userInfo.nickname}</label> <br />
+      <label>{userInfo.email}</label> <br />
+      <label>{userInfo.point}</label> <br />
 
-      <button onClick={() => {
-        navigate("/modify")
-      }}>
-        수정
+      <button onClick={() => navigate("/modify")}>
+      수정
       </button>
-      <button className="deleteId" onClick={() => {
-        axiosInstance.delete('/delete')
-          .then(response => {
-            console.log(response.data);
-            alert("탈퇴 하시겠습니까?")
-            navigate("/")
-          }) .catch(error => {
-            console.log(error);
-          })
-        
-      }}>탈퇴</button>
+
+      <button
+        className="deleteId"
+        onClick={() => {
+          if (!window.confirm("정말로 탈퇴하시겠습니까?")) return;
+
+          axiosInstance
+            .delete('/delete', { data: { id: userInfo.id } })
+            .then((response) => {
+              console.log(response.data);
+              alert("탈퇴가 완료되었습니다.");
+              navigate("/");
+            })
+            .catch((error) => {
+              console.error(error);
+              alert("탈퇴 중 오류가 발생했습니다.");
+            });
+        }}
+      >
+        탈퇴
+      </button>
     </div>
   )
 }
