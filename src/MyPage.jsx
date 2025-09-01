4e0539a393a8cc124e103cad28c976319d4599f8
panicbaek <woosikbaek@gmail.com>
2025-09-01 12:47:27 +0900

    테두리 랭크 이미지 업로드

--- src/MyPage.jsx
+++ src/MyPage.jsx
@@ -1,20 +1,28 @@
-
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axiosInstance from "../axiosInstance";
 
-const MyPage = ({ userInfo, setUserInfo, auth }) => {
+const MyPage = ({ userInfo, setUserInfo, auth, setAuth }) => {
 
   const navigate = useNavigate();
 
-  return (
+    const logout = () => {
+    sessionStorage.removeItem('jwt');
+    setAuth(false);
+    setUserInfo('');
+  }
 
+  return (
     <div className="login-container">
-      <p>{userInfo.nickname}님의 마이페이지</p>
+      <h2>{userInfo.username}님</h2>
       <hr />
-      <label>{userInfo.nickname}</label> <br />
-      <label>{userInfo.email}</label> <br />
-      <label>{userInfo.point}</label> <br />
+      <label>닉네임 : {userInfo.nickname}</label> <br />
+      <label>이메일 : {userInfo.email}</label> <br />
+      <label>포인트 : {userInfo.point}</label> <br />
+      <label>롤점수 : {userInfo.score}</label> <br />
+      <label>배그점수 : {userInfo.score}</label> <br />
+      <label>스타점수 : {userInfo.score}</label> <br />
+      <label>메이플점수 : {userInfo.score}</label> <br />
 
       <button onClick={() => navigate("/modify")}>
       수정
@@ -23,13 +31,12 @@ const MyPage = ({ userInfo, setUserInfo, auth }) => {
       <button
         className="deleteId"
         onClick={() => {
-          if (!window.confirm("정말로 탈퇴하시겠습니까?")) return;
-
+          const pw = window.prompt("한번더 비밀번호를 입력해 주세요");
           axiosInstance
-            .delete('/delete', { data: { id: userInfo.id } })
+            .delete('/delete', { params: { password: pw } })
             .then((response) => {
-              console.log(response.data);
-              alert("탈퇴가 완료되었습니다.");
+              alert(response.data);
+              logout();
               navigate("/");
             })
             .catch((error) => {
@@ -41,7 +48,6 @@ const MyPage = ({ userInfo, setUserInfo, auth }) => {
         탈퇴
       </button>
     </div>
-
   )
 }
 
