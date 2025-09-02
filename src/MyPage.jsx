d4828b7cf7e79ab1928d0a21f9cbae2d6940078a
altjs3310 <altjs3310@naver.com>
2025-09-02 18:00:36 +0900

    재수정

--- src/MyPage.jsx
+++ src/MyPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axiosInstance from "../axiosInstance";
+import './css/Mypage.css'
 
 const MyPage = ({ userInfo, setUserInfo, auth, setAuth }) => {
 
