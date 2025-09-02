431367d7d16c7848ed7736353ef60a84078ce34f
panicbaek <woosikbaek@gmail.com>
2025-09-02 10:01:19 +0900

    메인페이지 추가

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,12 +1,13 @@
 import Slider from "./Slider";
 import MainContent from "./MainContent";
 import "./css/Mainpage.css"
+import Gameranking from "./GameRanking";
 const MainPage = () => {
   return (
     <>
       <Slider />
       <MainContent />
-    
+      <Gameranking />
     </>
   )
 }
