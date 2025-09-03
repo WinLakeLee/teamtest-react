4ca482007b3c594635d87e4bafe6b64cae493c04
altjs3310 <altjs3310@naver.com>
2025-09-03 12:49:53 +0900

    보완완료

--- src/Market.jsx
+++ src/Market.jsx
@@ -137,32 +137,33 @@ const Market = ({auth, setAuth, userInfo, setUserInfo}) => {
       </table>
     </div>          
 
-
-    <h3 className="market-title">내 아이템</h3>
-    <table className="item-grid">
-        <thead>
-          <tr>
-            <th>아이템명</th>
-            <th>가격</th>
-            <th>환불</th>
-            <th>사용</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Array.isArray(myItem) && myItem.map((myItem)=>(
-            <tr key={myItem.id} className="item-card">
-              <td className="item-name">{myItem.item.itemName}</td>
-              <td className="item-price">{myItem.item.itemPrice}</td>
-              <td className="buy-button">{myItem.item.itemCategory === "GRADE" ? (
-                <button disabled style={{backgroundColor: "rgba(230, 35, 35, 1)"}}>환불불가</button>
-              ) : ( 
-              <button onClick={() => handleRefund(myItem)}>환불</button>
-              )}</td>
-              <td className="buy-button"><button onClick={() => handleUseItem(myItem)}>사용하기</button></td>
-            </tr>  
-          ))}
-      </tbody>   
-    </table>
+    <div className="market-container">
+      <h3 className="market-title">내 아이템</h3>
+      <table className="item-grid">
+          <thead>
+            <tr>
+              <th>아이템명</th>
+              <th>가격</th>
+              <th>환불</th>
+              <th>사용</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Array.isArray(myItem) && myItem.map((myItem)=>(
+              <tr key={myItem.id} className="item-card">
+                <td className="item-name">{myItem.item.itemName}</td>
+                <td className="item-price">{myItem.item.itemPrice}</td>
+                <td className="buy-button">{myItem.item.itemCategory === "GRADE" ? (  // 등급 아이템일 경우 환불버튼 disabled
+                  <button disabled style={{backgroundColor: "rgba(230, 35, 35, 1)"}}>환불불가</button>
+                ) : ( 
+                <button onClick={() => handleRefund(myItem)}>환불</button>
+                )}</td>
+                <td className="buy-button"><button onClick={() => handleUseItem(myItem)}>사용하기</button></td>
+              </tr>  
+            ))}
+        </tbody>   
+      </table>
+    </div>
     </>
   );
 };
