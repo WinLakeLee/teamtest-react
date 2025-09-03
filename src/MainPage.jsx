721de0b2b503bbdfe3ccd451e8f86e8fb740c40c
panicbaek <woosikbaek@gmail.com>
2025-09-03 13:10:14 +0900

    사이드바

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -3,11 +3,13 @@ import MainContent from "./MainContent";
 import "./css/Mainpage.css"
 import Gameranking from "./GameRanking";
 import Sidebar from "./Sidebar";
+import RSidebar from "./RSidebar";
 
 const MainPage = () => {
   return (
     <>
       <Sidebar/>
+      <RSidebar/>
       <Slider />
       <MainContent />
       <Gameranking />
