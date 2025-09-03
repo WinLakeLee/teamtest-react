afe9ac6546ceee8e10359f8620415e5a4ec0e5a1
panicbaek <woosikbaek@gmail.com>
2025-09-03 10:03:14 +0900

    사이드바 추가

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -2,9 +2,12 @@ import Slider from "./Slider";
 import MainContent from "./MainContent";
 import "./css/Mainpage.css"
 import Gameranking from "./GameRanking";
+import Sidebar from "./Sidebar";
+
 const MainPage = () => {
   return (
     <>
+      <Sidebar/>
       <Slider />
       <MainContent />
       <Gameranking />
--- src/MyPage.jsx
+++ src/MyPage.jsx
@@ -5,10 +5,9 @@ import axiosInstance from "../axiosInstance";
 const MyPage = ({ userInfo, setUserInfo, auth, setAuth }) => {
 
   const navigate = useNavigate();
-
+  console.log(auth)
   const logout = () => {
     sessionStorage.removeItem('jwt');
-    setAuth(false);
     setUserInfo('');
   }
 
@@ -51,6 +50,7 @@ const MyPage = ({ userInfo, setUserInfo, auth, setAuth }) => {
                 .then((response) => {
                   console.log(response.data);
                   alert("탈퇴가 완료되었습니다.");
+                  logout();
                   navigate("/");
                 })
                 .catch((error) => {
